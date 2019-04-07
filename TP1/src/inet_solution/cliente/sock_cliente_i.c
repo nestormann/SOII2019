1329bc5b3dab7142d4c8a81773643de733cd9d4d
nestormann <nestormann96@gmail.com>
2019-04-07 03:29:46 -0300

    Fix de la consigna, anda lindo che.

--- TP1/src/inet_solution/cliente/sock_cliente_i.c
+++ TP1/src/inet_solution/cliente/sock_cliente_i.c
@@ -4,42 +4,61 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
 #include <unistd.h>
+#include <netdb.h> 
 #include <errno.h>
 #include <time.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <syslog.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <dirent.h>
+#include <getopt.h>
 
 #define TAM 4096
 
 int main( int argc, char *argv[] ) 
  {
+	clock_t start_time = clock(); 	
 	int sockfd, puerto, n;
 	int sock_udp;
 	socklen_t tamano_direccion;
 	struct sockaddr_in serv_addr;
 	struct sockaddr_in dest_addr;
 	struct hostent *server;
-	//struct hostent *server_udp;
-	int terminar = 0;
-	int descargar=0;
-	int correct_input = 0; 										
-	char *token;												 
-	char *usuario;
-	char input[TAM];
-	int correct_user=0;
-	int correct_pass=0;
+	int descargar=0;						
+	char *token;			
+	char buffer[TAM];
+	//char input[TAM];
+	int enviar=0;
+	//int sinc=0;
 	int file_size;
+	char file_sizee[256];
     FILE *received_file;
     int remain_data = 0;
 	int restart_flag=0;
-    ssize_t len;
     char file_path[100]="";
     char current_dir[100]="";
     getcwd(current_dir,100);
+	char file_to_send[100]="";
 	FILE *firmware;
-    printf("argc= %d\nargv= %s\n",argc,*argv);
     char firmware_path[100]="./firmware.bin";
-    char buff[255];
+	char current_directory[100]; 				/*Almacenamos el directorio actual*/
+	/*Datos del satelite*/
+    char ver_sat[255];
+	char id_sat[5]="R2D2";
+	char *host= (char*) malloc(1024); 
+	int fd;
+	struct stat file_stat;
+	//int sinc;
+	int sent_bytes = 0;
+	off_t offset;
+	ssize_t len;
+	gethostname(host, 1024);
+	char prompt[TAM];
+	sprintf(prompt, "%s@%s:~%s$ ",id_sat,host,getcwd(current_directory,100));
+
     firmware = fopen(firmware_path, "r");
     if (firmware == NULL)
      {
@@ -47,69 +66,13 @@ int main( int argc, char *argv[] )
      }
     else
      {
-        fgets(buff, 255, (FILE*)firmware);
-        printf("Version del software: %s\n",buff); 
+        fgets(ver_sat, 255, (FILE*)firmware);
+        printf("Version del software: %s\n",ver_sat);
+		printf("ID: %s\n", id_sat); 
         fclose(firmware);
-     }
-
-    /*Se comprueba la sintaxis del comando ingresado, 
-      de ser correcto prosigo, de lo contrario finaliza*/
-	while(correct_input==0)
-	 {
-
-		printf("LOGIN: "); 								
-		fgets(input,75,stdin);
-		//scanf("%s", input);							
-		input[strlen(input)-1]='\0';						
-		char delimit[3];									
-		delimit[0]=' ';
-		delimit[1]='@';
-		delimit[2]=':';
-		token= strtok(input, delimit);							
-
-		while(token==NULL || strncmp(token,"connect",7))
-		 {	
-			printf("Comando incorrecto, reingrese\n");
-			printf("LOGIN: "); 								
-			fgets(input,75,stdin);						
-			input[strlen(input)-1]='\0';
-			token= strtok(input, delimit);	
-		 }
-
-		token= strtok(NULL, delimit);
-		if(token==NULL)
-		 {
-			printf("No ingreso el usuario\n");
-			exit(0);
-		 }
-		else
-		 {
-			usuario=token;
-			token= strtok(NULL, delimit);					
-		 }
-
-		int index_argv=1;											
-		while(token != NULL)
-		{								
-			argv[index_argv]=token;
-			index_argv++;
-			argc=index_argv;
-			token= strtok(NULL, delimit);
-		 }
-		argv[index_argv]='\0';							
-
-		printf("Usuario ingresado: %s\n",usuario );
-		correct_input=1;
 	 }
 
-	/*Conexion con el puerto del servidor TCP*/
-	if ( argc < 3 ) 
-	 {
-		fprintf( stderr, "Uso %s host puerto\n", argv[0]);
-		exit( 0 );
-	 }
-
-	puerto = atoi( argv[2] );
+	puerto = atoi( "4040" );
 	sockfd = socket( AF_INET, SOCK_STREAM, 0 );
 	if ( sockfd < 0 ) 
 	 {
@@ -117,7 +80,7 @@ int main( int argc, char *argv[] )
 		exit( 1 );
 	 }
 
-	server = gethostbyname( argv[1] );
+	server = gethostbyname( "127.0.0.1" );
 	if (server == NULL) 
 	 {
 		fprintf( stderr,"Error, no existe el host\n" );
@@ -147,208 +110,228 @@ int main( int argc, char *argv[] )
 	memset( &(dest_addr.sin_zero), '\0', 8 );
 
 	while(1) 
-	 {	/*Espera autenticacion del usuario*/
-		while(correct_user==0)
-		 {
-			n = write( sockfd, usuario, strlen(usuario) );				
-			if ( n < 0 ) 
-			 {
-				perror( "escritura de socket" );
-				exit( 1 );
-			 }
-			/*Respuesta de comprobacion*/
-			memset( input, '\0', TAM );									
-			n = read( sockfd, input, TAM );
-			if ( n < 0 ) 
-			 {
-				perror( "lectura de socket" );
-				exit( 1 );
-			 }
-			if(strncmp(input,"wrongUser",9)==0)
-			 {						
-				printf( "Usuario desconocido\n" );
-				exit(0);
-			 }
-			/*Usuario correcto, envio la contraseña*/
-			if(strncmp(input,"correctUser",11)==0)
-			 {	
-				printf( "Usuario correcto, ingrese la contraseña: " );
-				memset( input, '\0', TAM );
-				fgets( input, TAM-1, stdin );
-				n = write( sockfd, input, strlen(input) );
-				if ( n < 0 ) 
-				 {
-					perror( "escritura de socket" );
-					exit( 1 );
-				 }
-				correct_user=1;
-			 }
-		 }
-		/*Espera de autenticacion de la contraseña*/
-		while(correct_pass==0)
-		 {	/*Respuesta de comprobacion*/
-			memset( input, '\0', TAM );									
-			n = read( sockfd, input, TAM );
-			if ( n < 0 ) 
-			 {
-				perror( "lectura de socket" );
-				exit( 1 );
-			 }
-			if(strncmp(input,"wrongPass",9)==0)
-			 {	
-				printf( "Contraseña incorrecta, reingrese: " );
-				memset( input, '\0', TAM );
-				fgets( input, TAM-1, stdin );
-				n = write( sockfd, input, strlen(input) );
-				if ( n < 0 ) 
-				 {
-					perror( "escritura de socket" );
-					exit( 1 );
-				 }
-			 }
-			if(strncmp(input,"correctPass",11)==0)
-			 {	
-				printf( "Contraseña correcta, conexion establecida\n" );
-				correct_pass=1;
-			 }
-		 }
-		/*Procedimiento de descarga*/
-		if(descargar==1)
-		 {
-			/* Receiving file size */
-			memset( input, '\0', TAM );
-	        recv(sockfd, input, TAM, 0);
-
-			if(strncmp(input,"nofile",6)==0)
-			 {
-			 	printf("NO EXISTE EL ARCHIVO SOLICITADO\n");
-			 	descargar=0;
-			 }
-			else
-			 {
-			 	printf("DESCARGANDO\n");
-		        file_size = atoi(input);
-		        fprintf(stdout, "File size : %d\n", file_size);
-
-		        received_file = fopen(file_path, "w");
-		        if (received_file == NULL)
-		         {
-		            fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
-		            exit(EXIT_FAILURE);
-		         }
-
-		        remain_data = file_size;
-
-				clock_t start = clock();
-
-		        memset( input, '\0', TAM );					
-		        while (((len = recv(sockfd, input, TAM, 0)) > 0) && (remain_data >=0))
-		         {
-		            fwrite(input, sizeof(char), len, received_file);
-		            remain_data -= len;
-		            if(remain_data==0) 
-		            	break;
-		         }
-
-				clock_t end = clock();
-				float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-		        printf("TERMINO DESCARGA EN %f segundos\n",seconds);
-		        fclose(received_file);
-		        /*Envio confirmacion de fin de descarga*/
-				n = write( sockfd, "done", 5);
-				if ( n < 0 ) 
-				{
-					perror( "escritura de socket" );
-					exit( 1 );
-				}
-				if(restart_flag)
-				{
-					execvp(argv[0],argv);
-				}
-				descargar=0;
-			 }
-		 }
-		
-		if(descargar==0)
-		 {
-			/*Mensaje de sincronizacion*/
-			write( sockfd, "SINC", 5);
-			if ( n < 0 ) 
-			{
-				perror( "escritura de socket" );
-				exit( 1 );
-			}
-	   	 }
-
-		/*Recepcion e impresion del prompt*/
-		memset( input, '\0', TAM );					
-		n = read( sockfd, input, TAM );
-		if ( n < 0 ) 
-		 {
-			perror( "lectura de socket" );
-			exit( 1 );
-		 }
-		printf("%s",input);	
-
-		/*Ingreso y envio del comando a ejecutar*/
-		memset( input, '\0', TAM );
-		fgets( input, TAM-1, stdin );
-		n = write( sockfd, input, strlen(input) );
-		if ( n < 0 ) 
-		 {
-			perror( "escritura de socket" );
-			exit( 1 );
-		 }
-
-		/*Verificacion de terminacion del programa*/
-		input[strlen(input)-1] = '\0';
-		if( !strcmp( "fin", input ) ) 
-		 {
-			terminar = 1;
-		 }
-		if( terminar ) 
-		 {
-			printf( "Finalizando ejecución\n" );
-			exit(0);
-		 }
-
-		/*Verificacion de intento de descargar*/
-		if(strncmp(input,"update firmware.bin",19)==0 || strncmp(input,"start scanning",14)==0)  
+	 {	
+		/*while(!sinc) 
 		{
-			if(strncmp(input,"update firmware.bin",19)==0) 
-			{
-				printf("Me voy a cagar reiniciando\n");
-				restart_flag=1;
-			}
-			descargar = 1;
-			strtok(input, " ");	
-			token=strtok(NULL," ");		/*Nombre del archivo a descargar*/
-			sprintf(file_path, "%s/%s", current_dir, token);
-			
-		}
-		else
-		{	/*Recepcion de salida del servidor*/
-			memset( input, '\0', TAM );					
-			n = read( sockfd, input, TAM );
+				
+			sinc=1;
+		}*/
+		/*Procedimiento de envio*/
+				if(enviar==1 && descargar==0)
+				 {
+					fd = open(file_to_send, O_RDONLY);
+        			if (fd == -1)
+			         {
+			            fprintf(stderr, "Error opening file --> %s", strerror(errno));
+			            n = write( sockfd,"nofile" , 6);		
+						if ( n < 0 ) 
+						{
+							perror( "escritura en socket" );
+							exit( 1 );
+						}
+						enviar=0;
+			         }
+			        else
+			         { 
+				        if (fstat(fd, &file_stat) < 0)
+				         {
+				            fprintf(stderr, "Error fstat --> %s", strerror(errno));
+				            exit(EXIT_FAILURE);
+				         }
+
+				        fprintf(stdout, "File Size: \n%zd bytes\n", file_stat.st_size);
+
+				        //clilen = sizeof(struct sockaddr_in);
+
+				         /* Sending file size */
+				        sprintf(file_sizee, "%zd", file_stat.st_size);
+				        len = send(sockfd, file_sizee, sizeof(file_sizee), 0);
+
+				        if (len < 0)
+				         {
+				              fprintf(stderr, "Error on sending greetings --> %s", strerror(errno));
+				              exit(EXIT_FAILURE);
+				         }
+
+				        fprintf(stdout, "Server sent %zd bytes for the size\n", len);
+
+				        offset = 0;
+				        remain_data = file_stat.st_size;
+
+				        /* Sending file data */
+				        while (((sent_bytes = sendfile(sockfd, fd, &offset, TAM)) > 0) && (remain_data >= 0) )
+				         {
+				            remain_data -= sent_bytes;
+				         }
+
+				        printf("FINALIZO DESCARGA\n");
+				        /*Espera confirmacion para finalizar la descarga*/
+				        memset( buffer, 0, TAM );
+						n = read( sockfd, buffer, TAM-1 );
+						if ( n < 0 ) 
+						 {
+							perror( "lectura de socket" );
+							exit(1);
+						 }
+
+					    if(strcmp("done", buffer)==0) 
+						{
+					    	enviar=0;
+							if(restart_flag)
+							{
+								printf("fin de conexion\n");
+								exit(0);
+							} 
+						}
+
+						
+					 }
+				 }
+		
+		/*Procedimiento de descarga*/
+		if(descargar==1 && enviar==0)
+				{
+					/* Receiving file size */
+					memset( buffer, '\0', TAM );
+					recv(sockfd, buffer, TAM, 0);
+
+					if(strncmp(buffer,"nofile",6)==0)
+					{
+						printf("NO EXISTE EL ARCHIVO SOLICITADO\n");
+						descargar=0;
+					}
+					else
+					{
+						printf("DESCARGANDO\n");
+						file_size = atoi(buffer);
+						fprintf(stdout, "File size : %d\n", file_size);
+
+						received_file = fopen(file_path, "w");
+						if (received_file == NULL)
+						{
+							fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
+							exit(EXIT_FAILURE);
+						}
+
+						remain_data = file_size;
+
+						clock_t start = clock();
+						
+
+						memset( buffer, '\0', TAM );
+						n = write( sockfd, "sinc", 5);
+						if ( n < 0 ) 
+						{
+							perror( "escritura en socket" );
+							exit( 1 );
+						}					
+						while (((len = recv(sockfd, buffer, TAM, 0)) > 0) && (remain_data >=0))
+						{
+							fwrite(buffer, sizeof(char), len, received_file);
+							remain_data -= len;
+							if(remain_data==0) 
+								break;
+						}
+
+						clock_t end = clock();
+						float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+						printf("TERMINO DESCARGA EN %f segundos\n",seconds);
+						fclose(received_file);
+						/*Envio confirmacion de fin de descarga*/
+						n = write( sockfd, "done", 5);
+						if ( n < 0 ) 
+						{
+							perror( "escritura de socket" );
+							exit( 1 );
+						}
+						if(restart_flag)
+						{
+							execvp(argv[0],argv);
+						}
+						descargar=0;
+					}
+				}
+		/*Envio del prompt a mostar en el cliente*/
+		if(descargar==0 && enviar==0)
+		{	
+			/*Recepcion de sinc*/
+			memset( buffer, '\0', TAM );					
+			n = read( sockfd, buffer, TAM );
 			if ( n < 0 ) 
 			{
 				perror( "lectura de socket" );
 				exit( 1 );
 			}
-			printf("%s\n",input);
-			
-			if (strncmp(input,"TELEMETRIA",10)==0) 
-			 {
+			printf("%s\n",prompt);
+			n = write( sockfd,prompt , strlen(prompt));	
+			if ( n < 0 ) 
+			{
+				perror( "escritura en socket" );
+				exit( 1 );
+			}
+		}
+
+		/*Espera de ingreso de comandos*/
+		memset( buffer, 0, TAM );								
+		n = read( sockfd, buffer, TAM-1 );
+		if ( n < 0 ) 
+			{
+			perror( "lectura de socket" );
+			exit(1);
+			}
+		buffer[strlen(buffer)-1] = '\0';
+		printf( "Recibí el siguiente comando: %s\n", buffer );
+
+		/*Intento de ejecucion de comandos*/
+		if(descargar==0 && enviar==0)
+		{
+			if(strncmp(buffer,"start scanning",14)==0)
+			{
+				printf("Envio scanning\n");
+				enviar=1;
+				getcwd(file_to_send,100);		/*Obtengo el path de descarga*/
+				strtok(buffer, " ");			/*Descarto el primer eslabon*/
+				token=strtok(NULL," ");			/*Obtengo el nombre del archivo a descargar*/
+				sprintf(file_to_send, "%s/%s", current_directory, token);
+			}
+			else if(strncmp(buffer,"update firmware.bin",19)==0) 
+			{
+				restart_flag=1;
+				descargar = 1;
+				strtok(buffer, " ");	
+				token=strtok(NULL," ");		/*Nombre del archivo a descargar*/
+				sprintf(file_path, "%s/%s", current_dir, token);
+			}
+			else if (strncmp(buffer,"obtener telemetria",18)==0) 
+			{
 				printf("Enviando telemetria\n");
+				clock_t current_time = clock(); 
+				float uptime = (float)(current_time - start_time) / CLOCKS_PER_SEC;	
 				tamano_direccion = sizeof( dest_addr );
-				n = sendto( sock_udp, (void *)buff, TAM, 0, (struct sockaddr *)&dest_addr, tamano_direccion );
+				sprintf(ver_sat, "%s/%f", ver_sat, uptime);
+				n = sendto( sock_udp, (void *)ver_sat, TAM, 0, (struct sockaddr *)&dest_addr, tamano_direccion );
 				if ( n < 0 ) 
 				{
 					perror( "Escritura en socket" );
 					exit( 1 );
 				}
-			 }	
-		}
+			}
+			else if(strncmp(buffer,"fin",3)==0)
+			{
+				printf( "PROCESO %d. Como recibí 'fin', termino la ejecución.\n\n", getpid() );
+				exit(0);
+			}
+			else
+			{
+				printf("Comando desconocido\n");
+				n = write( sockfd, buffer, strlen(buffer));
+				if ( n < 0 ) 
+				{
+					perror( "escritura en socket" );
+					exit( 1 );
+				}
+			}
+		}	
 	 }
 	return 0;
  } 
--- TP1/src/inet_solution/servidor/sock_servidor_i.c
+++ TP1/src/inet_solution/servidor/sock_servidor_i.c
@@ -5,10 +5,11 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <netdb.h>
-#include <sys/wait.h>
 #include <errno.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 #include <syslog.h>
 #include <sys/stat.h>
 #include <sys/sendfile.h>
@@ -28,24 +29,32 @@ int main( int argc, char *argv[] )
 	int n;
 	char usuario[]="nestor";
 	char password[]="parapam";
-	int user_verification=0;
-	int pass_verification=0;
+	int user_verification=1;
+	int pass_verification=1;
+	char file_path[100]="";
+    char current_dir[100]="";
+    getcwd(current_dir,100);
+	int intentos=0;
 	char current_directory[100]; 				/*Almacenamos el directorio actual*/
 	char *host= (char*) malloc(1024); 
 	gethostname(host, 1024);
 	char prompt[TAM];
 	sprintf(prompt, "%s@%s:~%s$ ",usuario,host,getcwd(current_directory,100));
 	int enviar=0;
+	int descargar=0;
 	int fd;
 	struct stat file_stat;
+	//int sinc;
 	int sent_bytes = 0;
 	off_t offset;
 	ssize_t len;
-	char file_size[256];
+	char file_sizee[256];
 	int remain_data;
 	char file_to_send[100]="";
 	int restart_flag=0;
     char *token;
+	int file_size;
+	FILE *received_file;
     /*char *argv_to_baash[10];*/
 	if ( argc < 2 ) 
 	 {
@@ -102,7 +111,32 @@ int main( int argc, char *argv[] )
 	tamano_direccion = sizeof( struct sockaddr );
 
 	while( 1 ) 
-	 {	int newsockfd, pid;
+	 {	
+		/*
+			Autenticacion
+		*/
+		while(pass_verification!=0 || user_verification!=0)
+		{	
+			if(intentos==3)
+			{
+				printf("FALLO EN LA AUTENTICACION, FIN DEL PROGRAMA\n");
+				exit(1);
+			}
+			else if (intentos>0) 
+			{
+				printf("Usuario y/o pass incorrecto, reintento %d de 2\n",intentos);
+			}
+		
+			printf("INGRESE EL USUARIO: ");
+			fgets(buffer,75,stdin);
+			user_verification=strncmp(buffer,usuario,strlen(buffer)-1);
+			printf("INGRESE LA CONTRASEÑA: ");
+			fgets(buffer,75,stdin);
+			pass_verification=strncmp(buffer,password,strlen(buffer)-1);
+			intentos++;
+		} 
+		printf("Espera de conexion con el satelite\n");
+		int newsockfd, pid;
 		newsockfd = accept( sockfd, (struct sockaddr *) &cli_addr, &clilen );
 		if ( newsockfd < 0 ) 
 		 {
@@ -122,76 +156,9 @@ int main( int argc, char *argv[] )
 			close( sockfd );
 
 			while ( 1 ) 
-			 {	/*Verificacion del usuario*/
-				while(user_verification==0)
-				 {										
-					memset( buffer, 0, TAM );
-					n = read( newsockfd, buffer, TAM-1 );
-					if ( n < 0 ) 
-					 {
-						perror( "lectura de socket" );
-						exit(1);
-					 }
-					user_verification=strncmp(buffer,usuario,strlen(usuario));
-					if(user_verification!=0)
-					 {
-						printf("usuario incorrecto, matando proceso %d\n",getpid());
-						n = write( newsockfd, "wrongUser", 10 );
-						if ( n < 0 ) 
-						 {
-							perror( "escritura en socket" );
-							exit( 1 );
-						 }
-						exit(0);
-					 }
-					else
-					 {
-						printf("Usuario correcto\n");
-						n = write( newsockfd, "correctUser", 12);
-						if ( n < 0 ) 
-						 {
-							perror( "escritura en socket" );
-							exit( 1 );
-						 }
-						user_verification=1;
-					 }
-				 }
-				/*Verificacion de la contraseña*/
-				while(pass_verification==0)
-				 {											
-					printf("Espera de contraseña\n");
-					memset( buffer, 0, TAM );
-					n = read( newsockfd, buffer, TAM-1 );
-					if ( n < 0 ) 
-					 {
-						perror( "lectura de socket" );
-						exit(1);
-					 }
-
-					if(strncmp(buffer,password,strlen(buffer)-1)!=0)
-					 {
-						printf("Contraseña incorrecta, reintento\n");
-						n = write( newsockfd, "wrongPass", 10 );
-						if ( n < 0 ) 
-						 {
-							perror( "escritura en socket" );
-							exit( 1 );
-						 }
-					 }
-					else
-					 {
-						printf("Contraseña correcta, conexion establecida\n");
-						n = write( newsockfd, "correctPass", 12);
-						if ( n < 0 ) 
-						 {
-							perror( "escritura en socket" );
-							exit( 1 );
-						 }
-						pass_verification=1;
-					 }
-				 }
-				/*Procedimiento de descarga*/
-				if(enviar==1)
+			 {	
+				/*Procedimiento de envio*/
+				if(enviar==1 && descargar==0)
 				 {
 					fd = open(file_to_send, O_RDONLY);
         			if (fd == -1)
@@ -215,11 +182,11 @@ int main( int argc, char *argv[] )
 
 				        fprintf(stdout, "File Size: \n%zd bytes\n", file_stat.st_size);
 
-				        clilen = sizeof(struct sockaddr_in);
+				        //clilen = sizeof(struct sockaddr_in);
 
 				         /* Sending file size */
-				        sprintf(file_size, "%zd", file_stat.st_size);
-				        len = send(newsockfd, file_size, sizeof(file_size), 0);
+				        sprintf(file_sizee, "%zd", file_stat.st_size);
+				        len = send(newsockfd, file_sizee, sizeof(file_sizee), 0);
 
 				        if (len < 0)
 				         {
@@ -231,6 +198,15 @@ int main( int argc, char *argv[] )
 
 				        offset = 0;
 				        remain_data = file_stat.st_size;
+
+						/*Recibo el sinc*/
+						memset( buffer, 0, TAM );
+						n = read( newsockfd, buffer, TAM-1 );
+						if ( n < 0 ) 
+						{
+							perror( "lectura de socket" );
+							exit(1);
+						}
 
 				        /* Sending file data */
 				        while (((sent_bytes = sendfile(newsockfd, fd, &offset, TAM)) > 0) && (remain_data >= 0) )
@@ -262,9 +238,74 @@ int main( int argc, char *argv[] )
 					 }
 				 }
 				
-				if(enviar==0)
+				/*Procedimiento de descarga*/
+				if(descargar==1 && enviar==0)
 				{
-					/*Recibo mensaje de SINC*/
+					/* Receiving file size */
+					memset( buffer, '\0', TAM );
+					recv(newsockfd, buffer, TAM, 0);
+
+					if(strncmp(buffer,"nofile",6)==0)
+					{
+						printf("NO EXISTE EL ARCHIVO SOLICITADO\n");
+						descargar=0;
+					}
+					else
+					{
+						printf("DESCARGANDO\n");
+						file_size = atoi(buffer);
+						fprintf(stdout, "File size : %d\n", file_size);
+
+						received_file = fopen(file_path, "w");
+						if (received_file == NULL)
+						{
+							fprintf(stderr, "Failed to open file foo --> %s\n", strerror(errno));
+							exit(EXIT_FAILURE);
+						}
+
+						remain_data = file_size;
+
+						clock_t start = clock();
+
+						memset( buffer, '\0', TAM );					
+						while (((len = recv(newsockfd, buffer, TAM, 0)) > 0) && (remain_data >=0))
+						{
+							fwrite(buffer, sizeof(char), len, received_file);
+							remain_data -= len;
+							if(remain_data==0) 
+								break;
+						}
+
+						clock_t end = clock();
+						float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+						printf("TERMINO DESCARGA EN %f segundos\n",seconds);
+						fclose(received_file);
+						/*Envio confirmacion de fin de descarga*/
+						n = write( newsockfd, "done", 5);
+						if ( n < 0 ) 
+						{
+							perror( "escritura de socket" );
+							exit( 1 );
+						}
+						if(restart_flag)
+						{
+							execvp(argv[0],argv);
+						}
+						descargar=0;
+					}
+				}
+				
+
+				if(enviar==0 && descargar==0)
+				{
+					/*Recibo el prompt*/
+					n = write( newsockfd, "SINC", 4 );
+					if ( n < 0 ) 
+					 {
+						perror( "escritura en socket" );
+						exit( 1 );
+				   	 }
+					/*Recibo el prompt*/
 					memset( buffer, 0, TAM );
 					n = read( newsockfd, buffer, TAM-1 );
 					if ( n < 0 ) 
@@ -272,81 +313,67 @@ int main( int argc, char *argv[] )
 						perror( "lectura de socket" );
 						exit(1);
 				 	 }
+					printf("%s",buffer);
 				}
 
-				/*Envio del prompt a mostar en el cliente*/
-				if(enviar==0)
-				 {
-					n = write( newsockfd,prompt , strlen(prompt));	
-					if ( n < 0 ) 
-					{
-						perror( "escritura en socket" );
+				/*Ingreso y envio del comando a ejecutar*/
+				memset( buffer, '\0', TAM );
+				fgets( buffer, TAM-1, stdin );
+				n = write( newsockfd, buffer, strlen(buffer) );
+				if ( n < 0 ) 
+				{
+					perror( "escritura de socket" );
+					exit( 1 );
+				}
+
+				/*Verificacion de terminacion del programa*/
+				buffer[strlen(buffer)-1] = '\0';
+				if( !strcmp( "fin", buffer ) ) 
+				{
+					printf( "Finalizando ejecución\n" );
+					exit(0);
+				}
+				/*Verificacion de intento de descargar*/
+				if(strncmp(buffer,"start scanning",14)==0)  
+				{
+					descargar = 1;
+					strtok(buffer, " ");	
+					token=strtok(NULL," ");		/*Nombre del archivo a descargar*/
+					sprintf(file_path, "%s/%s", current_dir, token);
+				}
+				else if(strncmp(buffer,"update firmware.bin",19)==0)  
+				{
+					printf("ENVIO FIRMWARE\n");
+					enviar = 1;
+					getcwd(file_to_send,100);		/*Obtengo el path de descarga*/
+					strtok(buffer, " ");			/*Descarto el primer eslabon*/
+					token=strtok(NULL," ");			/*Obtengo el nombre del archivo a descargar*/
+					sprintf(file_to_send, "%s/%s", current_directory, token);
+					restart_flag=1;
+				}
+				else if (strncmp(buffer,"obtener telemetria",18)==0) 
+				{
+					memset( buffer, 0, TAM );
+					n = recvfrom( sock_udp, buffer, TAM-1, 0, (struct sockaddr *)&serv_addr_udp, &tamano_direccion );
+					if ( n < 0 ) {
+						perror( "lectura de socket" );
 						exit( 1 );
 					}
-				 }
-
-				/*Espera de ingreso de comandos*/
-				memset( buffer, 0, TAM );								
-				n = read( newsockfd, buffer, TAM-1 );
-				if ( n < 0 ) 
-				 {
-					perror( "lectura de socket" );
-					exit(1);
-				 }
-				buffer[strlen(buffer)-1] = '\0';
-				printf( "Recibí el siguiente comando: %s\n", buffer );
-
-				/*Intento de ejecucion de comandos*/
-				if(enviar==0)
-				 {
-					if(strncmp(buffer,"update firmware.bin",19)==0 || strncmp(buffer,"start scanning",14)==0)
-					 {
-						if(strncmp(buffer,"update firmware.bin",19)==0) 
-						{
-							printf("Actualizacion del satelite\n");
-							restart_flag=1;
-						} 
-						printf("Envio actualizacion de firmware\n");
-						enviar=1;
-						getcwd(file_to_send,100);		/*Obtengo el path de descarga*/
-						strtok(buffer, " ");			/*Descarto el primer eslabon*/
-						token=strtok(NULL," ");			/*Obtengo el nombre del archivo a descargar*/
-						sprintf(file_to_send, "%s/%s", current_directory, token);
-						
-					 }
-					else if (strncmp(buffer,"obtener telemetria",18)==0) 
-					 {
-						printf("Esperando telemetria:\n");
-						n = write( newsockfd, "TELEMETRIA", 11 );
-						if ( n < 0 ) 
-						 {
-							perror( "escritura en socket" );
-							exit( 1 );
-						 }
-						memset( buffer, 0, TAM );
-						n = recvfrom( sock_udp, buffer, TAM-1, 0, (struct sockaddr *)&serv_addr_udp, &tamano_direccion );
-						if ( n < 0 ) {
-							perror( "lectura de socket" );
-							exit( 1 );
-						}
-						printf( "Version: %s\n", buffer );
-					 }
-					else if(strncmp(buffer,"fin",3)==0)
+					printf( "Version: %s\n", buffer );
+				}
+				
+				else
+				{	
+					/*Recepcion de salida del servidor*/
+					memset( buffer, '\0', TAM );					
+					n = read( newsockfd, buffer, TAM );
+					if ( n < 0 ) 
 					{
-						printf( "PROCESO %d. Como recibí 'fin', termino la ejecución.\n\n", getpid() );
-						exit(0);
+						perror( "lectura de socket" );
+						exit( 1 );
 					}
-					else
-					{
-						printf("Comando desconocido\n");
-						n = write( newsockfd, buffer, strlen(buffer));
-						if ( n < 0 ) 
-						 {
-							perror( "escritura en socket" );
-							exit( 1 );
-						 }
-					}
-				 }				
+					printf("%s\n",buffer);	
+				}
 			 }
 		 }
 		else 
