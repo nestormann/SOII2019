79a8a9b77598cc76286aab7358f090673919af36
nestormann <nestormann96@gmail.com>
2019-05-05 11:28:43 -0300

    Anda bien, ya imprime bien y todo. Falta OpenMPearlo

--- TP2/src/solution.c
+++ TP2/src/solution.c
@@ -9,6 +9,11 @@
 #include <netcdf_mem.h>
 #include <sys/stat.h>
 #include <assert.h>
+#include <sys/time.h>
+#include <time.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <getopt.h>
 #include "zlib.h"
 
 #ifdef NAN
@@ -32,139 +37,138 @@
  * 2712 , 3616 , 5424 , 7232 , 10848 , 21696
  * */
 #define total_steps 1 
-#define count_val NX/total_steps-1
+#define count_val NX/total_steps
 
-/* A coloured pixel. */
+// /* A coloured pixel. */
 
-typedef struct {
-    // uint8_t red;
-    // uint8_t green;
-    // uint8_t blue;
-    float alpha;
-} pixel_t;
+// typedef struct {
+//     // uint8_t red;
+//     // uint8_t green;
+//     // uint8_t blue;
+//     float alpha;
+// } pixel_t;
 
-/* A picture. */
+// /* A picture. */
 
-typedef struct {
-    pixel_t *pixels;
-    size_t width;
-    size_t height;
-} bitmap_t;
+// typedef struct {
+//     pixel_t *pixels;
+//     size_t width;
+//     size_t height;
+// } bitmap_t;
 
-/* Given "bitmap", this returns the pixel of bitmap at the point
-("x", "y"). */
+// /* Given "bitmap", this returns the pixel of bitmap at the point
+// ("x", "y"). */
 
-static pixel_t * pixel_at(bitmap_t * bitmap, int x, int y)
-{
-    return bitmap->pixels + y + bitmap->height * x;
-}
+// static pixel_t * pixel_at(bitmap_t * bitmap, int x, int y)
+// {
+//     return bitmap->pixels + y + bitmap->height * x;
+// }
 
-/* Write "bitmap" to a PNG file specified by "path"; returns 0 on
-success, non-zero on error. */
+// /* Write "bitmap" to a PNG file specified by "path"; returns 0 on
+// success, non-zero on error. */
 
-static int save_png_to_file(bitmap_t *bitmap, const char *path)
-{
-    FILE * fp;
-    png_structp png_ptr = NULL;
-    png_infop info_ptr = NULL;
-    size_t x, y;
-    png_byte ** row_pointers = NULL;
-    /* "status" contains the return value of this function. At first
-    it is set to a value which means 'failure'. When the routine
-    has finished its work, it is set to a value which means
-    'success'. */
-    int status = -1;
-    /* The following number is set by trial and error only. I cannot
-    see where it it is documented in the libpng manual.
-    */
-    int pixel_size = 4;
-    int depth = 8;
+// static int save_png_to_file(bitmap_t *bitmap, const char *path)
+// {
+//     FILE * fp;
+//     png_structp png_ptr = NULL;
+//     png_infop info_ptr = NULL;
+//     size_t x, y;
+//     png_byte ** row_pointers = NULL;
+//     /* "status" contains the return value of this function. At first
+//     it is set to a value which means 'failure'. When the routine
+//     has finished its work, it is set to a value which means
+//     'success'. */
+//     int status = -1;
+//     /* The following number is set by trial and error only. I cannot
+//     see where it it is documented in the libpng manual.
+//     */
+//     int pixel_size = 4;
+//     int depth = 8;
 
-    fp = fopen(path, "wb");
-    if (!fp) {
-        goto fopen_failed;
-    }
+//     fp = fopen(path, "wb");
+//     if (!fp) {
+//         goto fopen_failed;
+//     }
 
-    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (png_ptr == NULL) {
-        goto png_create_write_struct_failed;
-    }
+//     png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+//     if (png_ptr == NULL) {
+//         goto png_create_write_struct_failed;
+//     }
 
-    info_ptr = png_create_info_struct(png_ptr);
-    if (info_ptr == NULL) {
-        goto png_create_info_struct_failed;
-    }
+//     info_ptr = png_create_info_struct(png_ptr);
+//     if (info_ptr == NULL) {
+//         goto png_create_info_struct_failed;
+//     }
 
-    /* Set up error handling. */
+//     /* Set up error handling. */
 
-    if (setjmp(png_jmpbuf(png_ptr))) {
-        goto png_failure;
-    }
+//     if (setjmp(png_jmpbuf(png_ptr))) {
+//         goto png_failure;
+//     }
 
-    /* Set image attributes. */
+//     /* Set image attributes. */
 
-    png_set_IHDR(png_ptr,
-        info_ptr,
-        bitmap->width,
-        bitmap->height,
-        depth,
-        PNG_COLOR_TYPE_GRAY,
-        PNG_INTERLACE_NONE,
-        PNG_COMPRESSION_TYPE_DEFAULT,
-        PNG_FILTER_TYPE_DEFAULT);
+//     png_set_IHDR(png_ptr,
+//         info_ptr,
+//         bitmap->width,
+//         bitmap->height,
+//         depth,
+//         PNG_COLOR_TYPE_GRAY,
+//         PNG_INTERLACE_NONE,
+//         PNG_COMPRESSION_TYPE_DEFAULT,
+//         PNG_FILTER_TYPE_DEFAULT);
 
-    /* Initialize rows of PNG. */
+//     /* Initialize rows of PNG. */
 
-    row_pointers = png_malloc(png_ptr, bitmap->height * sizeof(png_byte *));
-    for (y = 0; y < bitmap->height; ++y) {
-        png_byte *row =
-            png_malloc(png_ptr, sizeof(uint8_t) * bitmap->width * pixel_size);
-        row_pointers[y] = row;
-        for (x = 0; x < bitmap->width; ++x) {
-            pixel_t * pixel = pixel_at(bitmap, x, y);
-            // *row++ = pixel->red;
-            // *row++ = pixel->green;
-            // *row++ = pixel->blue;
-            *row++ = pixel->alpha;
-        }
-    }
+//     row_pointers = png_malloc(png_ptr, bitmap->height * sizeof(png_byte *));
+//     for (y = 0; y < bitmap->height; ++y) {
+//         png_byte *row =
+//             png_malloc(png_ptr, sizeof(uint8_t) * bitmap->width * pixel_size);
+//         row_pointers[y] = row;
+//         for (x = 0; x < bitmap->width; ++x) {
+//             pixel_t * pixel = pixel_at(bitmap, x, y);
+//             // *row++ = pixel->red;
+//             // *row++ = pixel->green;
+//             // *row++ = pixel->blue;
+//             *row++ = pixel->alpha;
+//         }
+//     }
 
-    /* Write the image data to "fp". */
+//     /* Write the image data to "fp". */
 
-    png_init_io(png_ptr, fp);
-    png_set_rows(png_ptr, info_ptr, row_pointers);
-    png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+//     png_init_io(png_ptr, fp);
+//     png_set_rows(png_ptr, info_ptr, row_pointers);
+//     png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
 
-    /* The routine has successfully written the file, so we set
-    "status" to a value which indicates success. */
+//     /* The routine has successfully written the file, so we set
+//     "status" to a value which indicates success. */
 
-    status = 0;
+//     status = 0;
 
-    for (y = 0; y < bitmap->height; y++) {
-        png_free(png_ptr, row_pointers[y]);
-    }
-    png_free(png_ptr, row_pointers);
+//     for (y = 0; y < bitmap->height; y++) {
+//         png_free(png_ptr, row_pointers[y]);
+//     }
+//     png_free(png_ptr, row_pointers);
 
-png_failure:
-png_create_info_struct_failed:
-    png_destroy_write_struct(&png_ptr, &info_ptr);
-png_create_write_struct_failed:
-    fclose(fp);
-fopen_failed:
-    return status;
-}
+// png_failure:
+// png_create_info_struct_failed:
+//     png_destroy_write_struct(&png_ptr, &info_ptr);
+// png_create_write_struct_failed:
+//     fclose(fp);
+// fopen_failed:
+//     return status;
+// }
 
-/* Given "value" and "max", the maximum value which we expect "value"
-to take, this returns an integer between 0 and 255 proportional to
-"value" divided by "max". */
+// /* Given "value" and "max", the maximum value which we expect "value"
+// to take, this returns an integer between 0 and 255 proportional to
+// "value" divided by "max". */
 
-static int pix(float value, float max)
-{
-    if (value < 0)
-        return 0;
-    return (int)(256.0 *((float)(value) / (float)max));
-}
-
+// static int pix(float value, float max)
+// {
+//     if (value < 0)
+//         return 0;
+//     return (int)(256.0 *((float)(value) / (float)max));
+// }
 
 int
 main()
@@ -175,6 +179,7 @@ main()
      * */
 
     struct stat buffer;
+    struct timespec starting, end;
     int status;
     status = stat(FILE_NAME, &buffer);
     size_t size_file=buffer.st_size;
@@ -208,37 +213,56 @@ main()
      * */
     static size_t start[2]={0,0};
     static size_t count[2]={count_val,count_val};
+    clock_gettime(CLOCK_MONOTONIC_RAW, &starting);
+
     printf("Primera data_in[%ld][%ld] hasta data_in[%ld][%ld]\n",start[0],start[1],
                                                                 start[0]+count[0],start[1]+count[1]);
     if ((retval = nc_get_vara_float(ncid, varid,start,count, data_in)))
         ERR(retval);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    u_int64_t delta_us = (end.tv_sec - starting.tv_sec) * 1000000 + (end.tv_nsec - starting.tv_nsec) / 1000;
+    u_int64_t total_time_s = delta_us/1000000;
+    u_int64_t total_time_ms = (delta_us%1000000)/1000;
+    printf("CARGAR DATOS DE CMI: EN %lds %ldms %ldus\n",total_time_s, total_time_ms, delta_us%1000);
 
-    /**
-     * imprimo la imagen aca 
-     * */
-    bitmap_t data_in_img;
-    /* Create an image. */
-    data_in_img.width = NX;
-    data_in_img.height = NY;
-    data_in_img.pixels = calloc(sizeof(pixel_t), data_in_img.width * data_in_img.height);
-    for (int y = 0; y < data_in_img.height; y++) 
-    {
-        for (int x = 0; x < data_in_img.width; x++) 
-        {
-            pixel_t * pixel = pixel_at(&data_in_img, x, y);
-            pixel->alpha = pix(data_in[x+y*NY], 3832.0);
-        }
-    }
-    /* Write the image to a file 'data_in_img.png'. */
-    save_png_to_file(&data_in_img, "data_in_img.png");
+    // for(size_t i = 0; i < NX*NY; i++)
+    // {
+    //     if(data_in[i]==-1.0) data_in[i]=(float)0.0/0.0;
+    // }
+    
+    // /**
+    //  * imprimo la imagen aca 
+    //  * */
+    // clock_gettime(CLOCK_MONOTONIC_RAW, &starting);
+    // bitmap_t data_in_img;
+    // /* Create an image. */
+    // data_in_img.width = NX;
+    // data_in_img.height = NY;
+    // data_in_img.pixels = calloc(sizeof(pixel_t), data_in_img.width * data_in_img.height);
+    // for (int y = 0; y < data_in_img.height; y++) 
+    // {
+    //     for (int x = 0; x < data_in_img.width; x++) 
+    //     {
+    //         pixel_t * pixel = pixel_at(&data_in_img, x, y);
+    //         pixel->alpha = pix(data_in[x+y*NY], 3832.0);
+    //     }
+    // }
+    // /* Write the image to a file 'data_in_img.png'. */
+    // save_png_to_file(&data_in_img, "data_in_img.png");
+    // clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    // delta_us = (end.tv_sec - starting.tv_sec) * 1000000 + (end.tv_nsec - starting.tv_nsec) / 1000;
+    // total_time_s = delta_us/1000000;
+    // total_time_ms = (delta_us%1000000)/1000;
+    // printf("IMAGEN ORIGINAL: EN %lds %ldms %ldus\n",total_time_s, total_time_ms, delta_us%1000);
 
     /**
      * Convolucion
      * */
     float kernel[3][3] = {{-1,-1,-1},{-1,8,-1},{-1,-1,-1}};
-    // float data_in[] = {10,2,2,2,2,2,10,2,2,2,2,2,10,2,2,2,2,2,10,2,2,2,2,2,10};
     float *convolution = malloc((NX-2) * (NY-2) * sizeof(float));
-    for(int i=1; i < NX-1; i++)              
+    
+    clock_gettime(CLOCK_MONOTONIC_RAW, &starting);
+	for(int i=1; i < NX-1; i++)              
     {   
         for(int j=1; j < NY-1; j++)
         {
@@ -253,21 +277,45 @@ main()
                                                 data_in[(i+1)*NY+(j+1)]*kernel[2][2];               
         }
     }
-    bitmap_t convolution_img;
-    /* Create an image. */
-    convolution_img.width = NX-2;
-    convolution_img.height = NY-2;
-    convolution_img.pixels = calloc(sizeof(pixel_t), convolution_img.width * convolution_img.height);
-    for (int y = 0; y < convolution_img.height; y++) 
+	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    delta_us = (end.tv_sec - starting.tv_sec) * 1000000 + (end.tv_nsec - starting.tv_nsec) / 1000;
+    total_time_s = delta_us/1000000;
+    total_time_ms = (delta_us%1000000)/1000;
+    printf("CONVOLUCION: EN %lds %ldms %ldus\n",total_time_s, total_time_ms, delta_us%1000);
+
+    for (int y = 10000; y < 10005; y++) 
     {
-        for (int x = 0; x < convolution_img.width; x++) 
+        for (int x = 10000; x < 10005; x++) 
         {
-            pixel_t * pixel = pixel_at(&convolution_img, x, y);
-            pixel->alpha = pix(convolution[x+y*(NY-2)], 3832.0);
+            printf("%f  ",convolution[y*(NX-2)+x]);
         }
+        printf("\n");
     }
-    /* Write the image to a file 'convolution_img.png'. */
-    save_png_to_file(&convolution_img, "convolution_img.png");
+
+    
+    // clock_gettime(CLOCK_MONOTONIC_RAW, &starting);
+	// bitmap_t convolution_img;
+    // /* Create an image. */
+    // convolution_img.width = NX-2;
+    // convolution_img.height = NY-2;
+    // convolution_img.pixels = calloc(sizeof(pixel_t), convolution_img.width * convolution_img.height);
+    // for (int y = 0; y < convolution_img.height; y++) 
+    // {
+    //     for (int x = 0; x < convolution_img.width; x++) 
+    //     {
+    //         pixel_t * pixel = pixel_at(&convolution_img, x, y);
+    //         pixel->alpha = pix(convolution[x+y*(NY-2)], 5743.0);
+    //     }
+    // }
+    // /* Write the image to a file 'convolution_img.png'. */
+    // save_png_to_file(&convolution_img, "convolution_img.png");
+	// clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    // delta_us = (end.tv_sec - starting.tv_sec) * 1000000 + (end.tv_nsec - starting.tv_nsec) / 1000;
+    // total_time_s = delta_us/1000000;
+    // total_time_ms = (delta_us%1000000)/1000;
+    // printf("IMAGEN CONVOLUCION: EN %lds %ldms %ldus\n",total_time_s, total_time_ms, delta_us%1000);
+    
+    
 
     /* Se cierra el archivo y liberan los recursos*/
     if ((retval = nc_close(ncid)))
@@ -277,7 +325,7 @@ main()
 }
 
 
-    /**
+    // /**
     //  * Cual es el valor maximoooo
     //  * 
     //  * */
@@ -291,7 +339,5 @@ main()
     //     }
     // }
     // printf("Maximum value is %f.\n", maximum);
-
-    // find center position of kernel (half of kernel size)
 
 
