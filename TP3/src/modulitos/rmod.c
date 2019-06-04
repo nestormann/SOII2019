70af315a33419f5290ed5951a17cedc9cf3d023d
nestormann <nestormann96@gmail.com>
2019-06-04 20:13:18 -0300

    v1.0

--- TP3/src/modulitos/rmod.c
+++ TP3/src/modulitos/rmod.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv)
-{	
+{
+	/**
+	 * Abre el archivo donde se encuentra el nombre del modulo
+	 * cargado, lo lee y lo remueve.
+	 * */
+	char path[50] = "/var/www/html/nestor/uploads/filename.txt";
 	FILE *fp;
-	fp = fopen("filename.txt", "r"); // read mode
+	fp = fopen(path, "r");
 	if (fp == NULL)
 	{
-		perror("Error while opening the file.\n");
-		exit(EXIT_FAILURE);
+			printf("Error while opening the file.\n");
+			return 0;
 	}
-	char file_name[256];
-	fscanf(fp, "%s", file_name);
+	char *file_name;
+	char buff[256];
+	fscanf(fp, "%s", buff);
+	file_name = strtok(buff,".");
 	char command[256];
-	sprintf(command,"sudo rmmod /var/www/html/nestor/uploads/%s",file_name);
+	sprintf(command,"sudo rmmod %s",file_name);
 	system(command);
 	printf("Modulo %s removido correctamente\n",file_name);
 	return 0;
